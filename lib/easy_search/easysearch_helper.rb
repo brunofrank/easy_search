001eb5130de10dcaadbe3fd7e6b6aa22c73e75e6
Bruno Frank Cordeiro <bfscordeiro@gmail.com>
2011-02-13 18:48:03 -0200

    Moved form to partial

--- lib/easy_search/easysearch_helper.rb
+++ lib/easy_search/easysearch_helper.rb
@@ -1,9 +1,5 @@
 module EasySearchHelper
   def search_form
-    search_form = "<form id='search-form' action='#{url_for(:controller => controller_name, :action => action_name)}' method='GET'>"
-    search_form += "<input type='text' name='query' value='#{params[:query]}'/>"
-    search_form += "<input type='submit' id='search-button' value='search'/>" 
-    search_form += "</form>"     
-    search_form.html_safe
+    render :file => File.dirname(__FILE__) + '/views/_search_form.html.erb'
   end
 end
